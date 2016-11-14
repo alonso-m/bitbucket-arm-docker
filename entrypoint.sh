c41a45192809af9bd54e68542d15410bcb26517d
Paul Thompson <pathompson@atlassian.com>
2016-11-14 13:32:21 +1100

    Add debug command for clarify. Update file mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 # Credit to codemedic NA for this file
 if [ "$UID" -eq 0 ]; then
+    echo "User is currently root. Will change directories to daemon control, then downgrade permission to daemon"
     mkdir -p ${BITBUCKET_HOME}/lib &&
         chmod -R 700 "${BITBUCKET_HOME}" &&
         chown -R ${RUN_USER}:${RUN_GROUP} "${BITBUCKET_HOME}"
