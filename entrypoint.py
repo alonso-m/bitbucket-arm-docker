1f585d7e3fe035c6c9d353647abd31a5e64b0546
Steve Smith <ssmith@atlassian.com>
2019-08-21 11:28:05 +1000

    DCD-569: Remove some unneeded helper functions.

--- entrypoint.py
+++ entrypoint.py
@@ -16,18 +16,6 @@ def set_perms(path, user, group, mode):
     shutil.chown(path, user=user, group=group)
     os.chmod(path, mode)
 
-# Setup Jinja2 for templating
-jenv = j2.Environment(
-    loader=j2.FileSystemLoader('/opt/atlassian/etc/'),
-    autoescape=j2.select_autoescape(['xml']))
-
-def gen_cfg(tmpl, target, env, user='root', group='root', mode=0o644):
-    logging.info(f"Generating {target} from template {tmpl}")
-    cfg = jenv.get_template(tmpl).render(env)
-    with open(target, 'w') as fd:
-        fd.write(cfg)
-    set_perms(target, user, group, mode)
-
 
 ######################################################################
 # Setup inputs and outputs
@@ -38,12 +26,6 @@ def gen_cfg(tmpl, target, env, user='root', group='root', mode=0o644):
 # for backwards compatability, if the new version is not set.
 env = {k.lower(): v
        for k, v in os.environ.items()}
-
-
-
-######################################################################
-# Generate all configuration files
-# TODO
 
 
 ######################################################################
